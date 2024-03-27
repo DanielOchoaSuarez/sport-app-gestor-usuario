706164e879f9d7748ea4bb2f6ab845384380a8bc
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-03-27 11:42:09 -0500

    Ajuste pruebas unitarias

--- tests/deporte/test_get_deporte.py
+++ tests/deporte/test_get_deporte.py
@@ -1,47 +1,60 @@
 import uuid
 import json
+import pytest
+import logging
+from faker import Faker
 from src.main import app
 from src.models.db import db_session
 from src.models.deporte import DeporteEntity
 
 
+logger = logging.getLogger(__name__)
+faker = Faker()
+
+
+@pytest.fixture(scope="class")
+def setup_data():
+    logging.info("Inicio TestGetDeporte")
+    deporte_random = DeporteEntity(nombre=faker.name())
+    db_session.add(deporte_random)
+    db_session.commit()
+    logging.info('deporte creado: ' + deporte_random.nombre)
+    yield deporte_random
+    logging.info("Fin TestGetDeporte")
+    db_session.delete(deporte_random)
+    db_session.commit()
+
+
+@pytest.mark.usefixtures("setup_data")
 class TestGetDeporte():
 
-    def test_get_deporte(self):
+    def test_get_deporte(self, setup_data: DeporteEntity):
         with app.test_client() as test_client:
-            id_atletismo = self._obtener_atletismo()
             response = test_client.get(
-                '/gestor-usuarios/deportes?id='+id_atletismo, follow_redirects=True)
+                '/gestor-usuarios/deportes?id='+str(setup_data.id), follow_redirects=True)
             response_json = json.loads(response.data)
 
             assert response.status_code == 200
             assert 'nombre' in response_json
-            assert response_json['nombre'] == 'Atletismo'
+            assert response_json['nombre'] == setup_data.nombre
 
     def test_get_deporte_no_existente(self):
         with app.test_client() as test_client:
-            id_atletismo = str(uuid.uuid4())
+            id_deporte = str(uuid.uuid4())
             response = test_client.get(
-                '/gestor-usuarios/deportes?id='+id_atletismo, follow_redirects=True)
+                '/gestor-usuarios/deportes?id='+id_deporte, follow_redirects=True)
             response_json = json.loads(response.data)
 
             assert response.status_code == 404
             assert 'error' in response_json
-            assert response_json['error'] == f'El deporte con id {id_atletismo} no existe'
+            assert response_json['error'] == 'resource_not_found'
 
     def test_get_deporte_id_invalido(self):
         with app.test_client() as test_client:
-            id_atletismo = 'uuid_invalido'
+            id_deporte = 'uuid_invalido'
             response = test_client.get(
-                '/gestor-usuarios/deportes?id='+id_atletismo, follow_redirects=True)
+                '/gestor-usuarios/deportes?id='+id_deporte, follow_redirects=True)
             response_json = json.loads(response.data)
 
             assert response.status_code == 400
             assert 'error' in response_json
-
-    def _obtener_atletismo(self):
-        deportes = db_session.query(DeporteEntity).all()
-
-        for item in deportes:
-            if item.nombre == 'Atletismo':
-                return str(item.id)
--- tests/deporte/test_get_deportes.py
+++ tests/deporte/test_get_deportes.py
@@ -1,14 +1,46 @@
 import json
+from typing import List
+import pytest
+import logging
+from faker import Faker
 from src.main import app
+from src.models.db import db_session
+from src.models.deporte import DeporteEntity
 
 
+logger = logging.getLogger(__name__)
+faker = Faker()
+
+
+@pytest.fixture(scope="class")
+def setup_data():
+    logging.info("Inicio TestGetDeportes")
+    deporte_random_1 = DeporteEntity(nombre=faker.name())
+    deporte_random_2 = DeporteEntity(nombre=faker.name())
+    db_session.add(deporte_random_1)
+    db_session.add(deporte_random_2)
+    db_session.commit()
+    lista_deportes: List[DeporteEntity] = [deporte_random_1, deporte_random_2]
+    for deporte in lista_deportes:
+        logging.info('deporte creado: ' + deporte.nombre)
+
+    yield lista_deportes
+
+    logging.info("Fin TestGetDeportes")
+    for deporte in lista_deportes:
+        logging.info('deporte eliminado: ' + deporte.nombre)
+        db_session.delete(deporte)
+    db_session.commit()
+
+
+@pytest.mark.usefixtures("setup_data")
 class TestGetDeportes():
 
-    def test_get_deportes(self):
+    def test_get_deportes(self, setup_data: List[DeporteEntity]):
         with app.test_client() as test_client:
             response = test_client.get(
                 '/gestor-usuarios/deportes', follow_redirects=True)
             response_json = json.loads(response.data)
 
             assert response.status_code == 200
-            assert len(response_json) >= 2
+            assert len(response_json) >= len(setup_data)
